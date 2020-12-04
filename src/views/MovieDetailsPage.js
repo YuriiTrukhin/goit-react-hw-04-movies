be96011971eaaf223efbb23544aa654833ff1685
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-12-04 19:14:37 +0200

    name

--- src/views/MovieDetailsPage.js
+++ src/views/MovieDetailsPage.js
@@ -4,6 +4,9 @@ import { Link, Route, BrowserRouter } from "react-router-dom";
 import Cast from "../components/Cast/Cast";
 import Reviews from "../components/Reviews/Reviews";
 import Card from "../components/Card/Card";
+import routes from "../routes";
+
+// import MoviesPage from "./MoviesPage";
 
 // import PropTypes from 'prop-types'
 
@@ -14,12 +17,20 @@ export default class MovieDetailsPage extends Component {
   state = {
     page: null,
   };
+
   async componentDidMount() {
     const response = await Axios.get(
       `https://api.themoviedb.org/3/movie/${this.props.match.params.movieId}?api_key=401d61f37c17d956a98039a1a0734109&language=en-US`
     );
     this.setState({ page: response.data });
   }
+  handleGoBack = () => {
+    const { state } = this.props.location;
+    if (state && state.from) {
+      return this.props.history.push(state.from);
+    }
+    this.props.history.push(routes.HomePage);
+  };
   render() {
     const { page } = this.state;
     return (
@@ -27,13 +38,14 @@ export default class MovieDetailsPage extends Component {
         {page && (
           <div className="main-card">
             <div className="back">
-              <Link to="/">Go back</Link>
+              <button type="button" onClick={this.handleGoBack}>
+                Назад
+              </button>
             </div>
-
             <Card {...page} />
             <div className="additional-information">
               <ul>
-              <p>Additional information</p>
+                <p>Additional information</p>
                 <li className="cast">
                   <Link to={`${this.props.match.url}/cast`}>Cast</Link>
                 </li>
@@ -42,6 +54,7 @@ export default class MovieDetailsPage extends Component {
                 </li>
               </ul>
               <Route
+                exact
                 path={`${this.props.match.path}/cast`}
                 render={(props) => <Cast {...props} id={this.state.page.id} />}
               />
--- src/views/MoviesPage.js
+++ src/views/MoviesPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Axios from "axios";
 import Search from "../components/Search/Search";
 import { Link } from "react-router-dom";
+import queryString from "query-string";
 
 // import PropTypes from 'prop-types'
 
@@ -12,14 +13,47 @@ export default class MoviesPage extends Component {
   state = {
     shows: [],
     query: "",
+    totalResults: 0,
+    error: false,
   };
-  async componentDidUpdate(prevProps, prevState) {
-    if (this.state.query !== prevState.query) {
-      const response = await Axios.get(
-        `https://api.themoviedb.org/3/search/movie?api_key=401d61f37c17d956a98039a1a0734109&query=${this.state.query}&language=en-US&page=1&include_adult=false`
-      );
+  async componentDidMount() {
+    const { query } = queryString.parse(this.props.location.search);
+    if (query) {
+      try {
+        const response = await Axios.get(
+          `https://api.themoviedb.org/3/search/movie?api_key=401d61f37c17d956a98039a1a0734109&query=${query}&language=en-US&page=1&include_adult=false`
+        );
 
-      this.setState({ shows: response.data.results });
+        this.setState({
+          totalResults: response.data.total_results,
+          shows: response.data.results,
+        });
+      } catch {
+        this.setState({
+          error: true,
+        });
+      }
+    }
+  }
+  async componentDidUpdate(prevProps, prevState) {
+    const { query: prevQuery } = queryString.parse(prevProps.location.search);
+    const { query: nextQuery } = queryString.parse(this.props.location.search);
+
+    if (nextQuery !== prevQuery) {
+      try {
+        const response = await Axios.get(
+          `https://api.themoviedb.org/3/search/movie?api_key=401d61f37c17d956a98039a1a0734109&query=${nextQuery}&language=en-US&page=1&include_adult=false`
+        );
+
+        this.setState({
+          totalResults: response.data.total_results,
+          shows: response.data.results,
+        });
+      } catch {
+        this.setState({
+          error: true,
+        });
+      }
     }
   }
 
@@ -34,18 +68,25 @@ export default class MoviesPage extends Component {
   };
 
   render() {
-    console.log(this.state.shows);
+    const { totalResults, query, error } = this.state;
+
     return (
       <div className="search">
         <Search changeAdress={this.changeAdress} />
-        <ul>
-          {this.state.shows.map((movie) => (
-            <li key={movie.id}>
-              <Link to={`${this.props.match.url}/${movie.id}`}> {movie.title || movie.name}</Link>
-            </li>
-          ))}
-        </ul>
-        {/* <h3>Nothing found for your request</h3> */}
+        {totalResults > 1 || error ? (
+          <ul>
+            {this.state.shows.map((movie) => (
+              <li key={movie.id}>
+                <Link to={{ pathname: `${this.props.match.url}/${movie.id}`, state: { from: this.props.location } }}>
+                  {" "}
+                  {movie.title || movie.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          (query !== "" || error) && <h3>Nothing found for your request</h3>
+        )}
       </div>
     );
   }
